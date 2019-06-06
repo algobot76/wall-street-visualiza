555e407c1a7efcb71af9b013182896f1511ab333
Kaitian Xie <xkaitian@gmail.com>
2019-06-06 15:38:19 -0700

    Use Semantic UI

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App'
 
+import 'semantic-ui-css/semantic.min.css'
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));
 });
