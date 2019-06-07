1f1b658b7a7d757f2da18a5b0a205bf3e2a15866
Kaitian Xie <xkaitian@gmail.com>
2019-06-07 15:20:12 -0700

    Reformat SearchBar

--- imports/ui/components/SearchBar.jsx
+++ imports/ui/components/SearchBar.jsx
@@ -1,39 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    state ={
-        query:'',
-        result:[]
-    }
+  state = {
+    query: '',
+    result: []
+  };
 
-    getInfo = () =>{
-        axios.get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
-            .then(({ data }) => {
-                this.setState({
-                    results: data.data // MusicGraph returns an object named data,
-                                       // as does axios. So... data.data
-                })
-            })
-    }
-
-    handleInputChange = () => {
+  getInfo = () => {
+    axios
+      .get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
+      .then(({ data }) => {
         this.setState({
-            query:this.searchbar.value
-        })
-    }
+          results: data.data // MusicGraph returns an object named data,
+          // as does axios. So... data.data
+        });
+      });
+  };
 
-    render() {
-        return (
-            <form>
-                <input
-                    placeholder="Search ..."
-                    ref={input => this.searchbar = input}
-                    onChange={this.handleInputChange}
-                />
-                <p>{this.state.query}</p>
-            </form>
-        )
-    }
+  handleInputChange = () => {
+    this.setState({
+      query: this.searchbar.value
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search ..."
+          ref={input => (this.searchbar = input)}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.query}</p>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
