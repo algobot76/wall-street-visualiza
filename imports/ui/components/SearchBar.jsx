fff4d8e7976d3dad185c8baed189e615f4a7cbed
Kaitian Xie <xkaitian@gmail.com>
2019-06-07 15:56:42 -0700

    Refactor searchbar

--- imports/ui/components/SearchBar.jsx
+++ imports/ui/components/SearchBar.jsx
@@ -1,40 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Search } from 'semantic-ui-react';
 
-class SearchBar extends Component {
-  state = {
-    query: '',
-    result: []
+function SearchBar() {
+  const [query, setQuery] = useState('');
+  const handleSearchChange = (e, { value }) => {
+    setQuery(value);
   };
 
-  getInfo = () => {
-    axios
-      .get(`${API_URL}?api_key=${API_KEY}&prefix=${this.state.query}&limit=7`)
-      .then(({ data }) => {
-        this.setState({
-          results: data.data // MusicGraph returns an object named data,
-          // as does axios. So... data.data
-        });
-      });
-  };
-
-  handleInputChange = () => {
-    this.setState({
-      query: this.searchbar.value
-    });
-  };
-
-  render() {
-    return (
-      <form>
-        <input
-          placeholder="Search ..."
-          ref={input => (this.searchbar = input)}
-          onChange={this.handleInputChange}
-        />
-        <p>{this.state.query}</p>
-      </form>
-    );
-  }
+  return <Search onSearchChange={handleSearchChange} value={query} />;
 }
 
 export default SearchBar;
