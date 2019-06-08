07b395c41415b24d65712e50c532fb3d30700e38
Kaitian Xie <xkaitian@gmail.com>
2019-06-08 12:23:01 -0700

    Refactor with react hooks and some small fixes

--- imports/ui/components/Chart.jsx
+++ imports/ui/components/Chart.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import PropTyps from 'prop-types';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
   LineChart,
   Line,
@@ -13,81 +13,75 @@ import {
 } from 'recharts';
 import data from '../assets/dummy_stock_prices';
 
-const initialState = { data };
-
-class Chart extends Component {
-  state = initialState;
-  render() {
-    const { data } = this.state;
-    return (
-      <div className="line-charts">
-        <p style={{ fontWeight: 'bold', fontSize: 25, textAlign: 'center' }}>
-          Stock Market Chart (2016)
-        </p>
-        <div className="line-chart-wrapper">
-          <LineChart
-            width={1000}
-            height={600}
-            data={data}
-            margin={{ top: 10, right: 100, bottom: 50, left: 50 }}
-          >
-            <CartesianGrid
-              vertical={false}
-              horizontal={false}
-              strokeDasharray="5 5"
-            />
-            <XAxis
-              dataKey="date"
-              label={{ value: 'Time', position: 'insideBottom', offset: 0 }}
-            />
-            <YAxis
-              domain={['auto', 'auto']}
-              label={{
-                value: 'Stock Price',
-                angle: -90,
-                position: 'insideLeft',
-                offset: -7
-              }}
-            />
-            <Tooltip
-              cursor={{ strokeDasharray: '3 3' }}
-              wrapperStyle={{
-                borderColor: 'blue',
-                boxShadow: '2px 2px 3px 0px rgb(204, 204, 204)'
-              }}
-              contentStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}
-              labelStyle={{ fontWeight: 'bold', color: 'black' }}
-              offset={'10'}
-            />
-            <Line
-              dataKey="price"
-              stroke="#ff7300"
-              activeDot={{ stroke: 'red', strokeWidth: 12, r: 2 }}
-              isAnimationActive={true}
-              animationBegin={'0'}
-              animationEasing={'linear'}
-            />
-            <Brush dataKey="date" startIndex={0}>
-              <AreaChart>
-                <CartesianGrid />
-                <YAxis hide domain={['auto', 'auto']} />
-                <Area
-                  dataKey="price"
-                  stroke="#ff7300"
-                  fill="#ff7300"
-                  dot={false}
-                />
-              </AreaChart>
-            </Brush>
-          </LineChart>
-        </div>
+function Chart() {
+  return (
+    <div className="line-charts">
+      <p style={{ fontWeight: 'bold', fontSize: 25, textAlign: 'center' }}>
+        Stock Market Chart (2016)
+      </p>
+      <div className="line-chart-wrapper">
+        <LineChart
+          width={1000}
+          height={600}
+          data={data}
+          margin={{ top: 10, right: 100, bottom: 50, left: 50 }}
+        >
+          <CartesianGrid
+            vertical={false}
+            horizontal={false}
+            strokeDasharray="5 5"
+          />
+          <XAxis
+            dataKey="date"
+            label={{ value: 'Time', position: 'insideBottom', offset: 0 }}
+          />
+          <YAxis
+            domain={['auto', 'auto']}
+            label={{
+              value: 'Stock Price',
+              angle: -90,
+              position: 'insideLeft',
+              offset: -7
+            }}
+          />
+          <Tooltip
+            cursor={{ strokeDasharray: '3 3' }}
+            wrapperStyle={{
+              borderColor: 'blue',
+              boxShadow: '2px 2px 3px 0px rgb(204, 204, 204)'
+            }}
+            contentStyle={{ backgroundColor: 'rgba(255, 255, 255, 0.8)' }}
+            labelStyle={{ fontWeight: 'bold', color: 'black' }}
+            offset={'10'}
+          />
+          <Line
+            dataKey="price"
+            stroke="#ff7300"
+            activeDot={{ stroke: 'red', strokeWidth: 12, r: 2 }}
+            isAnimationActive={true}
+            animationBegin={'0'}
+            animationEasing={'linear'}
+          />
+          <Brush dataKey="date" startIndex={0}>
+            <AreaChart>
+              <CartesianGrid />
+              <YAxis hide domain={['auto', 'auto']} />
+              <Area
+                dataKey="price"
+                stroke="#ff7300"
+                fill="#ff7300"
+                dot={false}
+              />
+            </AreaChart>
+          </Brush>
+        </LineChart>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 Chart.propTypes = {
-  data: PropTyps.arrayOf(PropTypes.object).isRequired
+  data: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default Chart;
