49c63164dbea1bdb45bdca8c38de546199f5684b
Kaitian Xie <xkaitian@gmail.com>
2019-06-18 17:07:25 -0700

    Delete a bunch of components

--- imports/ui/components/App.jsx
+++ imports/ui/components/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import Navbar from './Navbar';
-import { Grid } from 'semantic-ui-react';
-import Chart from './Chart';
-import logo from '../assets/logo.jpg';
-import data from '../assets/dummy_stock_prices';
 
 function App() {
   return (
     <div>
-      <Grid padded>
-        <Navbar logoPath={logo} menuItems={['Home', 'About']} />
-        <Chart data={data} title="Stock Market Chart (2016)" />
-      </Grid>
+
     </div>
   );
 }
