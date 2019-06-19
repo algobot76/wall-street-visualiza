761db281d2773fdc73227756e3eec81f0bea02d3
Kaitian Xie <xkaitian@gmail.com>
2019-06-19 13:25:50 -0700

    Update App and NavbarMenu

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import About from './pages/About';
+import Visualiza from './pages/Visualiza';
 
 import logo from './assets/logo.png';
 
@@ -13,6 +15,8 @@ const App = () => {
         <Navbar logo={logo} />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/visualiza" component={Visualiza} />
         </Switch>
       </div>
     </Router>
--- imports/ui/components/NavbarMenu.jsx
+++ imports/ui/components/NavbarMenu.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Search from './Search';
@@ -16,18 +17,25 @@ class NavbarMenu extends Component {
     return (
       <div className="navbar-menu">
         <div className="navbar-start">
-          <div className="navbar-item">Home</div>
-          <div className="navbar-item">About</div>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <div className="navbar-link">Company List</div>
-            <div className="navbar-dropdown is-right">
-              {names.map((name, idx) => (
-                <a key={idx} className="navbar-item">
-                  {name}
-                </a>
-              ))}
-            </div>
-          </div>
+          <Link to={'/'} className="navbar-item">
+            Home
+          </Link>
+          <Link to={'/about'} className="navbar-item">
+            About
+          </Link>
+          <Link to={'/visualiza'} className="navbar-item">
+            Visuliza
+          </Link>
+          {/*<div className="navbar-item has-dropdown is-hoverable">*/}
+          {/*  <div className="navbar-link">Company List</div>*/}
+          {/*  <div className="navbar-dropdown is-right">*/}
+          {/*    {names.map((name, idx) => (*/}
+          {/*      <a key={idx} className="navbar-item">*/}
+          {/*        {name}*/}
+          {/*      </a>*/}
+          {/*    ))}*/}
+          {/*  </div>*/}
+          {/*</div>*/}
         </div>
         <div className="navbar-end">
           <Search />
