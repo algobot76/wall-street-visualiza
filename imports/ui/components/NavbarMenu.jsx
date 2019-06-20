5e4bfb65817f678130a11ce1614e580472f75d4d
Kaitian Xie <xkaitian@gmail.com>
2019-06-19 23:32:48 -0700

    Remove unused import in NavbarMenu

--- imports/ui/components/NavbarMenu.jsx
+++ imports/ui/components/NavbarMenu.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
 
 import Search from './Search';
 
