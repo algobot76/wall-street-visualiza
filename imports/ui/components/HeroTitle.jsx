c4b2b3cc6f9a680ee2b5cd8f4dcb7b1a6ed9b1f6
Kaitian Xie <xkaitian@gmail.com>
2019-06-19 19:24:44 -0700

    Update HeroTitle

--- imports/ui/components/HeroTitle.jsx
+++ imports/ui/components/HeroTitle.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const HeroTitle = ({ title, subtitle, color = '' }) => {
+const HeroTitle = ({ title = '', subtitle = '', color = '' }) => {
   return (
     <section className={`hero ${color}`}>
       <div className="hero-body">
         <div className="container has-text-centered">
-          {title && <h1 className="title">{title}</h1>}
-          {subtitle && <h2 className="subtitle">{subtitle}</h2>}
+          <h1 className="title">{title}</h1>
+          <h2 className="subtitle">{subtitle}</h2>
         </div>
       </div>
     </section>
