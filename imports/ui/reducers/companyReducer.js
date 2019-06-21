a178d52feb5072719fc6b674d5da2303f947d2af
Kaitian Xie <xkaitian@gmail.com>
2019-06-21 16:31:45 -0700

    Add more companies

--- imports/ui/reducers/companyReducer.js
+++ imports/ui/reducers/companyReducer.js
@@ -3,10 +3,32 @@ import { FETCH_COMPANIES, SELECT_COMPANY, QUERY } from '../actions';
 import stocks from '../seeds/stocks';
 
 const initialState = {
-  names: ['AAPL', 'FB', 'MSFT', 'TSLA'],
+  names: [],
   selectedCompany: 'AAPL',
   data: stocks
 };
+const companies = [
+  'BAC',
+  'AAPL',
+  'BNS',
+  'FB',
+  'TSLA',
+  'TD',
+  'SBUX',
+  'SHOP',
+  'RY',
+  'NKE',
+  'NTGR',
+  'NFLX',
+  'GOOG',
+  'GS',
+  'GM',
+  'F',
+  'DIS',
+  'CM',
+  'BMO',
+  'BBY'
+];
 
 const companyReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -18,7 +40,7 @@ const companyReducer = (state = initialState, action) => {
     case FETCH_COMPANIES:
       return {
         ...state,
-        names: ['AAPL', 'FB', 'MSFT', 'TSLA']
+        names: companies.sort()
       };
     case QUERY:
       return {
