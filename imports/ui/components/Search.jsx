53fc50ceca2e7e0aea067192235e9a858fb12b14
Kaitian Xie <xkaitian@gmail.com>
2019-06-21 21:12:47 -0700

    Change width of Search

--- imports/ui/components/Search.jsx
+++ imports/ui/components/Search.jsx
@@ -45,61 +45,47 @@ class Search extends Component {
         <div className="level">
           <div className="level-right">
             <div className="level-item">
-              <form
-                onSubmit={e => {
-                  e.preventDefault();
-                  if (!input.value.trim()) {
-                    return;
-                  }
-                  dispatch(search(input.value));
-                  input.value = '';
-                }}
-              >
-                <div className="field has-addons">
-                  <div className="dropdown is-active">
-                    <div className="dropdown-trigger">
-                      <div className="control has-icons-left">
-                        <input
-                          ref={node => (input = node)}
-                          type="text"
-                          className="input"
-                          placeholder="Look for a company?"
-                          onChange={e => this.updateInputValue(e)}
-                          onFocus={() => this.handleInputOnFocus()}
-                          onBlur={() => this.handleInputOnBlur()}
-                        />
-                        <span className="icon is-left">
-                          <FontAwesomeIcon icon={faSearch} />
-                        </span>
+              <div className="field">
+                <div className="dropdown is-active">
+                  <div className="dropdown-trigger">
+                    <div className="control has-icons-left is-15rem-wide">
+                      <input
+                        ref={node => (input = node)}
+                        type="text"
+                        className="input"
+                        placeholder="Look for a company?"
+                        onChange={e => this.updateInputValue(e)}
+                        onFocus={() => this.handleInputOnFocus()}
+                        onBlur={() => this.handleInputOnBlur()}
+                      />
+                      <span className="icon is-left">
+                        <FontAwesomeIcon icon={faSearch} />
+                      </span>
+                    </div>
+                  </div>
+                  {this.state.inputOnFocus && (
+                    <div className="dropdown-menu is-15rem-wide">
+                      <div className="dropdown-content">
+                        {names
+                          .filter(name =>
+                            name.includes(this.state.inputValue.toUpperCase())
+                          )
+                          .map((name, idx) => (
+                            <div key={idx} className="dropdown-item">
+                              <a
+                                onClick={() => {
+                                  dispatch(selectCompany(name));
+                                }}
+                              >
+                                {name}
+                              </a>
+                            </div>
+                          ))}
                       </div>
                     </div>
-                    {this.state.inputOnFocus && (
-                      <div className="dropdown-menu">
-                        <div className="dropdown-content">
-                          {names
-                            .filter(name =>
-                              name.includes(this.state.inputValue.toUpperCase())
-                            )
-                            .map((name, idx) => (
-                              <div key={idx} className="dropdown-item">
-                                <a
-                                  onClick={() => {
-                                    dispatch(selectCompany(name));
-                                  }}
-                                >
-                                  {name}
-                                </a>
-                              </div>
-                            ))}
-                        </div>
-                      </div>
-                    )}
-                  </div>
-                  <div className="control">
-                    <button className="button is-dark">Search</button>
-                  </div>
+                  )}
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
