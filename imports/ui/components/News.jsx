05c3ca6144cde32390ac0154aa40ac2308528fcc
Kaitian Xie <xkaitian@gmail.com>
2019-06-24 07:24:02 -0700

    Rewrite News

--- imports/ui/components/News.jsx
+++ imports/ui/components/News.jsx
@@ -1,46 +1,42 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import NewsEntry from './NewsEntry';
 import { fetchHeadlines } from '../actions';
 
-class News extends Component {
-  componentDidMount() {
-    this.props.dispatch(fetchHeadlines());
-  }
+function News() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchHeadlines());
+  }, []);
 
-  render() {
-    const { data } = this.props;
-    return (
-      <section className="hero is-fullheight has-background-light">
-        <div className="hero-body">
-          <div className="columns is-multiline">
-            {data &&
-              data.map((entry, idx) => (
-                <div className="column is-12-tablet is-6-desktop is-4-widescreen">
-                  <NewsEntry
-                    key={idx}
-                    image={entry.image}
-                    title={entry.title}
-                    description={entry.description}
-                    url={entry.url}
-                  />
-                </div>
-              ))}
-          </div>
+  const data = useSelector(state => state.news.headlines);
+
+  return (
+    <section className="hero is-fullheight has-background-light">
+      <div className="hero-body">
+        <div className="columns is-multiline">
+          {data &&
+            data.map((entry, idx) => (
+              <div className="column is-12-tablet is-6-desktop is-4-widescreen">
+                <NewsEntry
+                  key={idx}
+                  image={entry.image}
+                  title={entry.title}
+                  description={entry.description}
+                  url={entry.url}
+                />
+              </div>
+            ))}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 News.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object)
 };
 
-const mapStateToProps = state => ({
-  data: state.news.headlines
-});
-
-export default connect(mapStateToProps)(News);
+export default News;
