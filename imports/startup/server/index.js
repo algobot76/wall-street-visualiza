5b942113c463b079af3a6b37415cbaba50c1b3bc
Kaitian Xie <xkaitian@gmail.com>
2019-07-02 23:03:05 -0700

    Update index file of server

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,10 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import Companies from '../../api/companies/companies';
 
+import Companies from '../../api/companies/companies';
+import './register-api';
 import companies from '../seeds/companies';
 
 Meteor.startup(() => {
   companies.forEach(company => {
-    Companies.insert({ symbol: company.sy });
+    Companies.insert({ symbol: company.symbol });
   });
 });
