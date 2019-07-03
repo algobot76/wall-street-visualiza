a09332ca0dbb21c057887748ff6b11e1a6c28120
Kaitian Xie <xkaitian@gmail.com>
2019-07-02 17:31:03 -0700

    Add seeds data of companies

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,31 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 import Companies from '../../api/companies';
 
-const symbols = [
-  'BAC',
-  'AAPL',
-  'BNS',
-  'FB',
-  'TSLA',
-  'TD',
-  'SBUX',
-  'SHOP',
-  'RY',
-  'NKE',
-  'NTGR',
-  'NFLX',
-  'GOOG',
-  'GS',
-  'GM',
-  'F',
-  'DIS',
-  'CM',
-  'BMO',
-  'BBY'
-];
+import companies from '../seeds/companies';
 
 Meteor.startup(() => {
-  symbols.forEach(symbol => {
-    Companies.insert({ symbol });
+  companies.forEach(company => {
+    Companies.insert({ symbol: company.sy });
   });
 });
