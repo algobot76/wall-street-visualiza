41cda8ceb70123a49686923fbff2e50ccc051976
Kaitian Xie <xkaitian@gmail.com>
2019-07-02 21:47:54 -0700

    Remove deny method of Companies

--- imports/api/companies.js
+++ imports/api/companies.js
@@ -1,16 +1,5 @@
 import { Mongo } from 'meteor/mongo';
 
 const Companies = new Mongo.Collection('companies');
-Companies.deny({
-  insert() {
-    return true;
-  },
-  update() {
-    return true;
-  },
-  finished() {
-    return true;
-  }
-});
 
 export default Companies;
