ea1a20785cfb6a035a89ef89aa595551837bec9b
Kaitian Xie <xkaitian@gmail.com>
2019-07-04 07:54:40 -0700

    Remove an unused import in Chart

--- imports/ui/components/Chart.jsx
+++ imports/ui/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import {
   LineChart,
