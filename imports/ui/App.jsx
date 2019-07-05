853ad44161c3aeaa5a7959dcf64f5ee49b06c7b9
Kaitian Xie <xkaitian@gmail.com>
2019-07-05 00:27:54 -0700

    Add Login system to App

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 import About from './pages/About';
 import Visualiza from './pages/Visualiza';
+import NotFound from './pages/NotFound';
 
 import logo from './assets/logo.png';
 
-class App extends Component {
-  componentDidMount() {
-    document.body.classList.add('has-navbar-fixed-top');
-  }
+let isAuthenticated = false;
+Tracker.autorun(() => {
+  isAuthenticated = !!Meteor.userId();
+});
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Navbar logo={logo} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/visualiza" component={Visualiza} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar logo={logo} />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <PrivateRoute path="/home" component={Home} />
+          <PrivateRoute path="/about" component={About} />
+          <PrivateRoute path="/visualiza" component={Visualiza} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
 }
 
 export default App;
