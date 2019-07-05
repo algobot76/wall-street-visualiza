99e0d61dd13cb6bb0ee0b077901afa61bb2d2545
Kaitian Xie <xkaitian@gmail.com>
2019-07-04 17:16:08 -0700

    Implement layout of Signup

--- imports/ui/components/Signup.jsx
+++ imports/ui/components/Signup.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 
 function Signup() {
   return (
@@ -7,8 +9,49 @@ function Signup() {
         <label htmlFor="name" className="label">
           Name
         </label>
+        <div className="control has-icons-left">
+          <input type="text" className="input" placeholder="e.g. John Doe" />
+          <span className="icon is-left">
+            <FontAwesomeIcon icon={faUser} />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label htmlFor="email" className="label">
+          Email
+        </label>
+        <div className="control has-icons-left">
+          <input
+            type="email"
+            className="input"
+            placeholder="e.g. john.doe@example.com"
+          />
+          <span className="icon is-left">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label htmlFor="password" className="label">
+          Password
+        </label>
+        <div className="control has-icons-left">
+          <input type="password" className="input" placeholder="Password" />
+          <span className="icon is-left">
+            <FontAwesomeIcon icon={faKey} />
+          </span>
+        </div>
+      </div>
+      <div className="field is-grouped is-grouped-centered">
         <div className="control">
-          <input type="text" className="input" placeholder="Your name" />
+          <button type="submit" className="button is-dark">
+            Sign up
+          </button>
+        </div>
+        <div className="control">
+          <button type="button" className="button is-dark">
+            Cancel
+          </button>
         </div>
       </div>
     </form>
