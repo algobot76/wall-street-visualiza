a07e838e18386369ebe8e4b433e19c0216ee0a0e
Kaitian Xie <xkaitian@gmail.com>
2019-07-05 00:15:06 -0700

    Add NotFound page

---
+++ imports/ui/pages/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <section className="hero is-light is-fullheight">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <div className="notification">
+            <h1 className="title">Oops! Page not found</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NotFound;
