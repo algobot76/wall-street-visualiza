42f0aa5609210de6ad159c496a2d3b6a0c904c92
Kaitian Xie <xkaitian@gmail.com>
2019-07-08 13:06:27 -0700

    Use hero in Dashboard

--- imports/ui/pages/Dashboard.jsx
+++ imports/ui/pages/Dashboard.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <section className="hero is-light is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Dashboard</h1>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Dashboard;
