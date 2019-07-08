76a41efbba8a16ee1f5f89ad0fbd7b61607efc2a
Kaitian Xie <xkaitian@gmail.com>
2019-07-08 13:50:43 -0700

    Better UI

--- imports/ui/components/AccountInfo.jsx
+++ imports/ui/components/AccountInfo.jsx
@@ -1,69 +1,93 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faKey, faUser } from '@fortawesome/free-solid-svg-icons';
 
+import styled from 'styled-components';
+
+const Buttons = styled.div`
+  margin-top: 1.2rem;
+`;
+
 function AccountInfo() {
+  const [canEdit, setCanEdit] = useState(false);
+
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
   return (
     <form className="container">
-      <div className="field">
-        <label htmlFor="name" className="label">
-          Name
-        </label>
-        <div className="control has-icons-left">
-          <input
-            ref={nameRef}
-            type="text"
-            className="input"
-            placeholder="e.g. John Doe"
-          />
-          <span className="icon is-left">
-            <FontAwesomeIcon icon={faUser} />
-          </span>
+      <fieldset disabled={!canEdit}>
+        <div className="field is-horizontal">
+          <div className="field-label is-normal">
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control has-icons-left">
+                <input ref={nameRef} type="text" className="input" />
+                <span className="icon is-left">
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="field">
-        <label htmlFor="email" className="label">
-          Email
-        </label>
-        <div className="control has-icons-left">
-          <input
-            ref={emailRef}
-            type="email"
-            className="input"
-            placeholder="e.g. john.doe@example.com"
-          />
-          <span className="icon is-left">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </span>
+        <div className="field is-horizontal">
+          <div className="field-label is-normal">
+            <label htmlFor="email" className="label">
+              Email
+            </label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control has-icons-left">
+                <input ref={emailRef} type="email" className="input" />
+                <span className="icon is-left">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="field">
-        <label htmlFor="password" className="label">
-          Password
-        </label>
-        <div className="control has-icons-left">
-          <input ref={passwordRef} type="password" className="input" />
-          <span className="icon is-left">
-            <FontAwesomeIcon icon={faKey} />
-          </span>
+        <div className="field is-horizontal">
+          <div className="field-label is-normal">
+            <label htmlFor="password" className="label">
+              Password
+            </label>
+          </div>
+          <div className="field-body">
+            <div className="field">
+              <div className="control has-icons-left">
+                <input ref={passwordRef} type="password" className="input" />
+                <span className="icon is-left">
+                  <FontAwesomeIcon icon={faKey} />
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="field is-grouped is-grouped-centered">
-        <div className="control">
-          <button type="submit" className="button is-dark">
-            Sign up
-          </button>
+      </fieldset>
+      <Buttons>
+        <div className="field is-horizontal">
+          <div className="field-label" />
+          <div className="field-body">
+            <div className="field">
+              <div className="control">
+                <button
+                  onClick={() => setCanEdit(true)}
+                  type="button"
+                  className="button is-dark"
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="control">
-          <button type="button" className="button is-dark">
-            Cancel
-          </button>
-        </div>
-      </div>
+      </Buttons>
     </form>
   );
 }
