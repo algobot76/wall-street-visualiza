349e71fdb307bc597825bc2ec22f0a7de294313f
Kaitian Xie <xkaitian@gmail.com>
2019-07-09 03:01:07 -0700

    Refactor App with withTracker

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Tracker } from 'meteor/tracker';
+import { withTracker } from 'meteor/react-meteor-data';
 
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
@@ -15,22 +15,27 @@ import NotFound from './pages/NotFound';
 import logo from './assets/logo.png';
 import history from './helpers/history';
 
-let isAuthenticated = false;
-Tracker.autorun(() => {
-  isAuthenticated = !!Meteor.userId();
-  // console.log(`is authenticated ${isAuthenticated}`);
-  // const unauthenticatedPages = ['/', 'signup'];
-  // const authenticatedPages = ['/home', '/about', '/visualiza'];
-  // const pathname = history.location.pathname;
-  // const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
-  // const isAuthenticatedPage = authenticatedPages.includes(pathname);
-  // const isNotFoundPage = !isUnauthenticatedPage && !isAuthenticatedPage;
-  // if (isUnauthenticatedPage && isAuthenticated) {
-  //   history.goBack();
-  // }
-});
+function App({ isAuthenticated }) {
+  function PrivateRoute({ component: Component, ...rest }) {
+    return (
+      <Route
+        {...rest}
+        render={props =>
+          isAuthenticated ? (
+            <Component {...props} />
+          ) : (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: { from: props.location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
 
-function App() {
   return (
     <Router history={history}>
       <div>
@@ -49,24 +54,8 @@ function App() {
   );
 }
 
-function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isAuthenticated ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/',
-              state: { from: props.location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-export default App;
+export default withTracker(() => {
+  return {
+    isAuthenticated: !!Meteor.userId()
+  };
+})(App);
