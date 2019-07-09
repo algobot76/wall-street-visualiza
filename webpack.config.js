59751602b19c091a0916c6aa9cd2d889bc7dc8d1
Kaitian Xie <xkaitian@gmail.com>
2019-07-09 16:08:40 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const meteorExternals = require('webpack-meteor-externals');
+
+const clientConfig = {
+  entry: './client/main.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/main.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  externals: [meteorExternals()],
+  devServer: {
+    hot: true
+  }
+};
+
+const serverConfig = {
+  entry: ['./server/main.js'],
+  target: 'node',
+  devServer: {
+    hot: true
+  },
+  externals: [meteorExternals()]
+};
+
+module.exports = [clientConfig, serverConfig];
