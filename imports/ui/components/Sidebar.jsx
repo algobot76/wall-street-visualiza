0f339c220e408b0c539cf545bff27280a32a1fac
Kaitian Xie <xkaitian@gmail.com>
2019-07-16 03:22:50 -0700

    Add sidebar scrollable

--- imports/ui/components/Sidebar.jsx
+++ imports/ui/components/Sidebar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { selectCompany } from '../actions';
+
+// TODO: Make Scroll's max-height equal to height of menu
+const Scroll = styled.div`
+  max-height: 30rem;
+  overflow: auto;
+`;
 
 function Sidebar({ names }) {
   const dispatch = useDispatch();
@@ -12,20 +19,22 @@ function Sidebar({ names }) {
     <div className="column is-4-tablet is-3-desktop is-2-widescreen">
       <aside className="menu">
         <p className="menu-label">Company List</p>
-        <ul className="menu-list">
-          {names.map((name, idx) => (
-            <li
-              key={idx}
-              onClick={() => {
-                dispatch(selectCompany(name));
-              }}
-            >
-              <a className={name === selectedCompany ? 'is-active' : ''}>
-                {name}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <Scroll>
+          <ul className="menu-list">
+            {names.map((name, idx) => (
+              <li
+                key={idx}
+                onClick={() => {
+                  dispatch(selectCompany(name));
+                }}
+              >
+                <a className={name === selectedCompany ? 'is-active' : ''}>
+                  {name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </Scroll>
       </aside>
     </div>
   );
