5601de7017134e345d15e1a46a3f4a2e8eca3950
Kaitian Xie <xkaitian@gmail.com>
2019-07-20 06:12:13 -0700

    Add NewsAPI (WIP)

---
+++ imports/vendor/NewsAPI.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const TOP_HEADLINES_URL = "https://newsapi.org/v2/top-headlines";
+
+export function getTopHeadlines() {
+  const category = 'business';
+  const country = 'us';
+  
+  return new Promise((resolve, reject) => {
+
+  });
+}
